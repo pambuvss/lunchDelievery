d99df80a9d3a470b762d43b43f0907f333181f79
Vitaliy Suprunov <pambuvss@gmail.com>
2019-01-22 21:55:23 +0300

    Add menu view and modified view of food (without photos).

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -6,6 +6,9 @@ class MenusController < ApplicationController
   	@restaurant_id = params[:restaurant_id]
   	@restaurant = Restaurant.find(@restaurant_id)
   	@food = FoodItem.by_date_and_restaurant_id(@date, @restaurant_id)
+  	@food_first = @food.where type_of_course: :first_course
+  	@food_main = @food.where type_of_course: :main_course
+  	@food_drink = @food.where type_of_course: :drink
   end
 
 end
