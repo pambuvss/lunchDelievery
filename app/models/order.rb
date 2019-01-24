f5fe66ef3f3abaebc5cd61a44771c66f447c00aa
Vitaliy Suprunov <pambuvss@gmail.com>
2019-01-24 13:38:32 +0300

    Add date validation in order.

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,7 +5,16 @@ class Order < ApplicationRecord
 
 	validates :count, :numericality => { :greater_than => 0 }, presence: true
 	validates :address, presence: true
-  	validates_associated :user
+	validates_associated :user
   	validates_associated :restaurant
+  	validate :valid_delivery_date?
+  	
+
+
+  	def valid_delivery_date?
+	    unless delivery_time.today?
+	      errors.add(:delivery_time, "date should be today!")
+	    end
+  	end
 
 end
