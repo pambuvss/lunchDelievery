0f9b9c4d17b65844d8b7ee193253e1588f55030f
Vitaliy Suprunov <pambuvss@gmail.com>
2019-01-26 20:35:26 +0300

    Fix munu's links.

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -2,7 +2,7 @@ class MenusController < ApplicationController
 
   def show
   	@date= Date.parse params[:date]
-  	@restaurant_id = params[:restaurant_id]
+  	@restaurant_id = params[:id]
   	@restaurant = Restaurant.find(@restaurant_id)
   	@food = FoodItem.by_date_and_restaurant_id(@date, @restaurant_id)
   	@food_first = @food.where type_of_course: :first_course
