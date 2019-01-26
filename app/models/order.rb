d0ff336567509ce6bff81b10d768d824f39adbf1
Vitaliy Suprunov <pambuvss@gmail.com>
2019-01-26 19:34:55 +0300

    Add less_than validation to order's count.

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,7 @@ class Order < ApplicationRecord
 	belongs_to :restaurant
 	has_many :orders_list
 
-	validates :count, :numericality => { :greater_than => 0 }, presence: true
+	validates :count, :numericality => { greater_than: 0, less_than: 1000 }, presence: true
 	validates :address, presence: true
 	validates_associated :user
   	validates_associated :restaurant
