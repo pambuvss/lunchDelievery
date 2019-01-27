c3b3c7dca6b793712a243595083bffd671d2b2ff
Vitaliy Suprunov <pambuvss@gmail.com>
2019-01-27 13:51:47 +0300

    Refactor seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,21 @@
-100.times do 
-	name = Faker::Name.name
-	email =  Faker::Internet.email
-	user = User.new(email: email, password: email, password_confirmation: email, name: name)
-	user.save
-end
+def create_restaurant_and_seller name
+    # for example, if name = "Best Fast Food" =>
+    # seller's account create with email and passw = "BestFastFood@ld.com"
 
-30.times do
-	restaurant_name =  Faker::Company.name
-    restaurant = Restaurant.new(name: restaurant_name)
+    restaurant = Restaurant.new(name: name)
     restaurant.save
-
-
-    email = (restaurant.name.sub ' ', '.') + '@ld.com'
-    name = Faker::Name.name 
-    seller = User.new(email: email, password: email, password_confirmation: email, 
-    	name: name, role: :seller, restaurant_id: restaurant.id)
+    email = name.tr('^A-Za-z0-9', '') + "@ld.com"
+    password = email
+    name = Faker::Name.name
+    seller = User.new(email: email, name: name, password: password,
+        password_confirmation: password, role: :seller, restaurant: restaurant)
     seller.save
 end
+
+names = ["Best Fast Food", "Blue Eye", "Black Burger", "Simple Food",
+         "Chinese Restaurant", "Zabe Restaurant", "XXL Foood", "Small Place",
+         "Large Place", "Food With Soul", "Smelly Pot", "Vegeterian Restaurant",
+         "Tasteless Food", "Red Onion", "Pink Dolphin", "Terrible pain",     
+         "Albatross", "Ku Ka Ke", "Green Piece",  "White Burger"]
+
+names.each { |name| create_restaurant_and_seller name}
